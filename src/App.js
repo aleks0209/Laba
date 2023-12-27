a6cfa9c183f9f7de14fac73f1fc2289ea728d2f5
aleks0209 <sahavoron.su@mail.ru>
2023-12-27 20:29:54 +0300

    chore: Updated dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Воронин А.В.";
+  const greeting = "Это тестовый проект Воронин А.В. ";
 
   return (
     <div className="App">
